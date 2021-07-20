35f7595e6ba47fabe4b54186b47427299a0e436e
SophiaLangheld <gaoxuefei55@gmail.com>
2021-07-20 08:51:23 +0200

    node module init

---
+++ app.js
@@ -0,0 +1,10 @@
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function subtract(num1, num2){
+    return num1 - num2;
+}
+
+let addResult = add(1, 2);
+console.log(`1 + 2 = ${addResult}`);
